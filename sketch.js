0902ec7f2765bc76b7ac0548d76d8ed47bfc113f
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-02-27 14:29:37 -0500

    Ok requires js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,19 @@
-const Alert = "This Is A Test Alert"
+const Alert ="Sketch.Js Loaded"
 
 function setup() {
-   alert(alert);
+   createCanvas(400, 400)
    console.log("Sketch.Js Starting Javascript Crap
    setTimeout(function(){
-      console.log(Making Vars);
+      console.log("Making Vars");
    }, 5000);
    setTimeout(function(){
       console.log("Made Vars");
    }, 5000);
+   setTimeout(function(){
+       console.log("Sketch.Js Loading");
+   }, 5000);
+   setTimeout(function(){
+       console.log("Sketch.Js Loaded");
+   }, 5000);
+   alert(Alert);
 }
