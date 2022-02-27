8078ae754f1a56bc3683d3049532d4e3ceb74885
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-02-27 14:24:03 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,12 @@
+const Alert = "This Is A Test Alert"
+
+function setup() {
+   alert(alert);
+   console.log("Sketch.Js Starting Javascript Crap
+   setTimeout(function(){
+      console.log(Making Vars);
+   }, 5000);
+   setTimeout(function(){
+      console.log("Made Vars");
+   }, 5000);
+}
